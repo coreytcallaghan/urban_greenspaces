acf77d75b2ec2efc5b41c44d3a2b2623678bd379
Corey Callaghan <callaghan.corey.t@gmail.com>
2018-06-06 14:17:34 +1000

    convert flat .txt file into sql db

---
+++ SQL/add_spatial_index.sql
@@ -0,0 +1,6 @@
+ALTER TABLE spatial_table ADD COLUMN pt POINT;
+SET SQL_SAFE_UPDATES = 0;
+UPDATE spatial_table SET pt=POINT(LONGITUDE, LATITUDE);
+ALTER TABLE spatial_table 
+CHANGE COLUMN pt pt GEOMETRY NOT NULL;
+CREATE SPATIAL INDEX ix_spatial_table_pt ON spatial_table(pt);
