114fcf7c2de938ec8364e3c871475fcc9dd4f229
Corey Callaghan <callaghan.corey.t@gmail.com>
2018-06-25 13:12:27 +1000

    convert big eBird data into spatial query file

--- SQL/add_spatial_index.sql
+++ SQL/add_spatial_index.sql
@@ -1,3 +1,9 @@
+# A SQL SCript used to make a spatial dataset of eBird_aus_data
+
+CREATE TABLE spatial_table
+SELECT DISTINCT `SAMPLING_EVENT_IDENTIFIER`, `LATITUDE`, `LONGITUDE`
+FROM `ebird_aus_data`;
+
 ALTER TABLE spatial_table ADD COLUMN pt POINT;
 SET SQL_SAFE_UPDATES = 0;
 UPDATE spatial_table SET pt=POINT(LONGITUDE, LATITUDE);
